91bcb3fa0d14899b5bd90036e7a5eff195669321
Robert Dyjas <15113729+robdy@users.noreply.github.com>
2021-12-18 10:25:42 +0100

    Add UnpinApp script

---
+++ scripts/UnpinApp.ps1
@@ -0,0 +1,17 @@
+param(
+	[Parameter(ValueFromPipeline = $true)]
+	[string[]]
+	$AppNames
+)
+
+begin {
+	# https://stackoverflow.com/a/45152368/9902555
+	function Unpin-App([string]$appname) {
+		((New-Object -Com Shell.Application).NameSpace('shell:::{4234d49b-0245-4df3-b780-3893943456e1}').Items() |
+		Where-Object { $_.Name -eq $appname }).Verbs() | Where-Object { $_.Name.replace('&', '') -match 'Unpin from taskbar' } | ForEach-Object { $_.DoIt() }
+	}
+} process {
+	foreach ($AppName in $AppNames) {
+		Unpin-App $AppName
+	}
+}
